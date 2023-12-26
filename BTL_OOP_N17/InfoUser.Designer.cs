cb5fad2533291f8ae36ff64a2e4f5806e0223f0d
Ha.LN216822 <Ha.LN216822@sis.hust.edu.vn>
2023-12-26 23:17:15 +0700

    form đổi mkform đổi pasword

--- BTL_OOP_N17/InfoUser.Designer.cs
+++ BTL_OOP_N17/InfoUser.Designer.cs
@@ -41,7 +41,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(182, 51);
+            this.label1.Location = new System.Drawing.Point(154, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(189, 22);
             this.label1.TabIndex = 0;
@@ -83,7 +83,7 @@
             this.button1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.Location = new System.Drawing.Point(137, 234);
+            this.button1.Location = new System.Drawing.Point(142, 200);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(81, 24);
             this.button1.TabIndex = 4;
@@ -95,7 +95,7 @@
             this.button2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
             this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button2.Location = new System.Drawing.Point(260, 235);
+            this.button2.Location = new System.Drawing.Point(259, 201);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(111, 23);
             this.button2.TabIndex = 5;
@@ -106,7 +106,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 328);
+            this.ClientSize = new System.Drawing.Size(471, 255);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
