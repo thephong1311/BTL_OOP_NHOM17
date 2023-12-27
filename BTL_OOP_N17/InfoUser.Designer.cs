cc818891a514c37ebc81dec83e3c7db0f1f3fed7
lenguyet-ha <lenguyetha14@gmail.com>
2023-12-27 09:41:33 +0700

    alo

--- BTL_OOP_N17/InfoUser.Designer.cs
+++ BTL_OOP_N17/InfoUser.Designer.cs
@@ -83,7 +83,7 @@
             this.button1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.Location = new System.Drawing.Point(142, 200);
+            this.button1.Location = new System.Drawing.Point(131, 200);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(81, 24);
             this.button1.TabIndex = 4;
