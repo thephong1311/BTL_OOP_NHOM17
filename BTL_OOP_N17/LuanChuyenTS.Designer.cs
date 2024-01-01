7f3485bfe474a60c0df21839978bea544e4d74cf
Ha.LN216822 <Ha.LN216822@sis.hust.edu.vn>
2024-01-01 21:00:12 +0700

    form luan chuyen

--- BTL_OOP_N17/LuanChuyenTS.Designer.cs
+++ BTL_OOP_N17/LuanChuyenTS.Designer.cs
@@ -67,7 +67,7 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(26, 56);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 45);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(268, 205);
             this.dataGridView1.TabIndex = 1;
