66de892fcc41a08fbb953d87893ed012d4f66082
Ha.LN216822 <Ha.LN216822@sis.hust.edu.vn>
2024-01-02 07:57:35 +0700

    form thêm PTN

---
+++ BTL_OOP_N17/AddPTN.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_OOP_N17
+{
+    public partial class AddPTN : Form
+    {
+        public AddPTN()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
