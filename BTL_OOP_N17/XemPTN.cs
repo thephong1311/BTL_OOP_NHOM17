b778a5017a8e5cc2dfa666e74bdca1b2f5ba5b6b
Ha.LN216822 <Ha.LN216822@sis.hust.edu.vn>
2024-01-06 20:45:08 +0700

    find và clear xem PTN

---
+++ BTL_OOP_N17/XemPTN.cs
@@ -0,0 +1,52 @@
+﻿using BTL_OOP_N17.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_OOP_N17
+{
+    public partial class XemPTN : UserControl
+    {
+
+        private SqlConnection con = new SqlConnection(ConnectionString.connectionString);
+        public XemPTN()
+        {
+            InitializeComponent();
+            dataGridView1.DataSource = infoPTNGridView();
+        }
+        public DataTable infoPTNGridView()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * from PHONGTHINGHIEM", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            return dt;
+        }
+        public DataTable findPTN()
+        {
+            SqlDataAdapter find = new SqlDataAdapter("SELECT * from PHONGTHINGHIEM WHERE MAPTN = " + txtFind.Text, con);
+            DataTable dt_find = new DataTable();
+            find.Fill(dt_find);
+            return dt_find;
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = findPTN();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFind.Text = "";
+            dataGridView1.DataSource = null;
+        }
+    }
+}
+
+ 
+
