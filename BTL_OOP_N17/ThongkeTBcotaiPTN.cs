3303d54ad0d18c5f6a7a218861b9ebbe0307a1bc
hin-2312 <Hien.NTN216826@sis.hust.edu.vn>
2024-01-06 18:31:53 +0700

    Bổ sung find+ clear ở form con ThongkeTBcotaiPTN

--- BTL_OOP_N17/ThongkeTBcotaiPTN.cs
+++ BTL_OOP_N17/ThongkeTBcotaiPTN.cs
@@ -30,8 +30,36 @@ namespace BTL_OOP_N17
         }
         public void ResetState()
         {
-            textBox1.Text = "";
+            txtFind.Text = "";
             dataGridView1.DataSource = infoTKGridView();
+        }
+        public DataTable findPTN()
+        {
+            string query = "SELECT * FROM KIEMKE WHERE MAPTN = @MaPTN";
+
+            using (SqlCommand command = new SqlCommand(query, con))
+            {
+                // Thêm tham số và đặt giá trị
+                command.Parameters.AddWithValue("@MaPTN", txtFind.Text);
+
+                // Thực hiện truy vấn
+                SqlDataAdapter find = new SqlDataAdapter(command);
+                DataTable dt_find = new DataTable();
+                find.Fill(dt_find);
+
+                return dt_find;
+            }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = findPTN();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtFind.Text = "";
+            dataGridView1.DataSource = null;
         }
     }
 }
