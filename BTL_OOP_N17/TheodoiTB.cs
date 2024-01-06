c78578642a106537fcfb93d1a657d3819cb622cf
hin-2312 <Hien.NTN216826@sis.hust.edu.vn>
2024-01-06 18:36:36 +0700

    Kết nối thêm form Thêm SC+ Thêm LC trong form Theo dõi TB

--- BTL_OOP_N17/TheodoiTB.cs
+++ BTL_OOP_N17/TheodoiTB.cs
@@ -110,10 +110,6 @@ namespace BTL_OOP_N17
             xemCacYCsuachua1.Visible = false;
         }
 
-        private void ycThanhly1_Load(object sender, EventArgs e)
-        {
-
-        }
 
         private void btnThemYCmuon_Click(object sender, EventArgs e)
         {
@@ -123,7 +119,14 @@ namespace BTL_OOP_N17
 
         private void btnThemSC_Click(object sender, EventArgs e)
         {
+            fAddYeucausuachuaTB f = new fAddYeucausuachuaTB();
+            f.ShowDialog();
+        }
 
+        private void btnThemLC_Click(object sender, EventArgs e)
+        {
+            fAddLuanChuyen f= new fAddLuanChuyen();
+            f.ShowDialog();
         }
     }
 }
