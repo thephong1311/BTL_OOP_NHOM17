374b69be23a30463016696afed752da9ce4281de
lenguyet-ha <lenguyetha14gmail.com>
2024-01-08 21:58:09 -0800

    xong chart thông kê mua và thống kê tiền trong PTN

---
+++ BTL_OOP_N17/ChartMua.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace BTL_OOP_N17
+{
+    partial class ChartMua
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.chartTKMuaTS = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chartTKMuaTS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartTKMuaTS
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartTKMuaTS.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartTKMuaTS.Legends.Add(legend2);
+            this.chartTKMuaTS.Location = new System.Drawing.Point(12, 12);
+            this.chartTKMuaTS.Name = "chartTKMuaTS";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Số yêu cầu mua";
+            this.chartTKMuaTS.Series.Add(series2);
+            this.chartTKMuaTS.Size = new System.Drawing.Size(402, 300);
+            this.chartTKMuaTS.TabIndex = 2;
+            this.chartTKMuaTS.Text = "chartTKMuaTS";
+            title2.Name = "Title1";
+            title2.Text = "THỐNG KÊ MUA";
+            this.chartTKMuaTS.Titles.Add(title2);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(437, 94);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // ChartMua
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 381);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chartTKMuaTS);
+            this.Name = "ChartMua";
+            this.Text = "ChartMua";
+            this.Load += new System.EventHandler(this.ChartMua_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartTKMuaTS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartTKMuaTS;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ BTL_OOP_N17/ChartSoTienTrongPTN.cs
@@ -0,0 +1,56 @@
+﻿using BTL_OOP_N17.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_OOP_N17
+{
+    public partial class ChartSoTienTrongPTN : Form
+    {
+        public ChartSoTienTrongPTN()
+        {
+            InitializeComponent();
+        }
+
+        private void ChartSoTienTrongPTN_Load(object sender, EventArgs e)
+        {
+            try
+            { 
+             string sql = "select MAPTN, SUM([GIANHAP]*[SOLUONGTS]) AS THANHTIENTS FROM TAISAN GROUP BY MAPTN";
+            using (SqlConnection connection = new SqlConnection(ConnectionString.connectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // Tạo DataTable và đọc dữ liệu từ reader
+                        DataTable dataTable = new DataTable();
+                        dataTable.Load(reader);
+
+                        // Đặt DataTable làm nguồn dữ liệu cho dataGridView1
+                        dataGridView1.DataSource = dataTable;
+
+                        // Thêm dữ liệu vào biểu đồ
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            chartMoneyinLab.Series["đồng"].Points.AddXY(row["MAPTN"].ToString(), Convert.ToInt32(row["THANHTIENTS"]));
+                        }
+                    }
+                }
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
